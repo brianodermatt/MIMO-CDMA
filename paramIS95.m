3818c72a454cfa6828054e7d7edf64f3f3a80d22
Brian Odermatt <brian.odermatt@intergga.ch>
2020-06-14 20:17:38 +0200

    vary channel length

--- paramIS95.m
+++ paramIS95.m
@@ -24,8 +24,8 @@ displaySnrRange = P.SNRRange + 10*log10(P.HadLen);
 % P.NumberRxAntennas  = 2;        % Number of receive antennas for MIMO
 % P.ChannelType       = 'Multipath';   % Since MIMO is implemented, only multipath is possible (inverting an all-1 matrix gives a singularity)
 % P.MIMODetectorType  = 'MMSE';
-% P.ChannelLength = 3;
-% P.RakeFingers = 3;
+% P.ChannelLength     = 3;
+% P.RakeFingers       = 3;
 % figure();
 % users = [1 2 3 10 64];
 % for i = 1:length(users)
@@ -42,20 +42,40 @@ displaySnrRange = P.SNRRange + 10*log10(P.HadLen);
 % legend('-DynamicLegend');
 
 %% second simulation: vary number of antennas
+% P.ChannelType       = 'Multipath';   % Since MIMO is implemented, only multipath is possible (inverting an all-1 matrix gives a singularity)
+% P.MIMODetectorType  = 'MMSE';
+% P.CDMAUsers         = 2;
+% P.ChannelLength     = 3;
+% P.RakeFingers       = 3;
+% figure();
+% antennas = [1,1; 2,2; 4,4; 2,4];  % [n_T, n_R], n_R must be bigger than n_T
+% for i = 1:size(antennas,1)
+%     nT = antennas(i,1);
+%     nR = antennas(i,2);
+%     P.NumberTxAntennas  = nT;        % Number of transmission antennas for MIMO
+%     P.NumberRxAntennas  = nR;        % Number of receive antennas for MIMO
+%     name = sprintf('Ch.Len.:%d; Users:%d; Fingers:%d; n_T:%d; n_R:%d Detector:%s',P.ChannelLength,P.CDMAUsers,P.RakeFingers,P.NumberTxAntennas,P.NumberRxAntennas,P.MIMODetectorType);
+%     semilogy(SNRRange,simulator(P),'DisplayName',name);
+%     hold on;
+% end
+% xlabel('SNR [dB]','FontSize',12,'FontWeight','bold');
+% ylabel('BER','FontSize',12,'FontWeight','bold');
+% xlim([min(SNRRange) max(SNRRange)]);
+% grid minor;
+% legend('-DynamicLegend');
+
+%% third simulation: channel length
 P.NumberTxAntennas  = 2;        % Number of transmission antennas for MIMO
 P.NumberRxAntennas  = 2;        % Number of receive antennas for MIMO
 P.ChannelType       = 'Multipath';   % Since MIMO is implemented, only multipath is possible (inverting an all-1 matrix gives a singularity)
 P.MIMODetectorType  = 'MMSE';
 P.CDMAUsers         = 2;
-P.ChannelLength = 3;
-P.RakeFingers = 3;
 figure();
-antennas = [1,1; 2,2; 4,4; 2,4];  % [n_T, n_R], n_R must be bigger than n_T
-for i = 1:size(antennas,1)
-    nT = antennas(i,1);
-    nR = antennas(i,2);
-    P.NumberTxAntennas  = nT;        % Number of transmission antennas for MIMO
-    P.NumberRxAntennas  = nR;        % Number of receive antennas for MIMO
+lengths = [1, 3, 6];
+for i = 1:length(lengths)
+    channelLength = lengths(i);
+    P.ChannelLength = channelLength;
+    P.RakeFingers = channelLength;
     name = sprintf('Ch.Len.:%d; Users:%d; Fingers:%d; n_T:%d; n_R:%d Detector:%s',P.ChannelLength,P.CDMAUsers,P.RakeFingers,P.NumberTxAntennas,P.NumberRxAntennas,P.MIMODetectorType);
     semilogy(SNRRange,simulator(P),'DisplayName',name);
     hold on;
