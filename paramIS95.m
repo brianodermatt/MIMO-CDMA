30bd50432a6f2f857bbafa71c777d434aa5e0760
Brian Odermatt <brian.odermatt@intergga.ch>
2020-06-14 21:01:29 +0200

    Compare different detector types

--- paramIS95.m
+++ paramIS95.m
@@ -87,17 +87,39 @@ displaySnrRange = P.SNRRange + 10*log10(P.HadLen);
 % legend('-DynamicLegend');
 
 %% fourth simulation: vary number of RAKE fingers
+% P.NumberTxAntennas  = 2;        % Number of transmission antennas for MIMO
+% P.NumberRxAntennas  = 2;        % Number of receive antennas for MIMO
+% P.ChannelType       = 'Multipath';   % Since MIMO is implemented, only multipath is possible (inverting an all-1 matrix gives a singularity)
+% P.MIMODetectorType  = 'MMSE';
+% P.ChannelLength     = 4;
+% P.CDMAUsers         = 2;
+% figure();
+% fingers = [1, 2, 3, 4];
+% for i = 1:length(fingers)
+%     finger = fingers(i);
+%     P.RakeFingers = finger;
+%     name = sprintf('Ch.Len.:%d; Users:%d; Fingers:%d; n_T:%d; n_R:%d Detector:%s',P.ChannelLength,P.CDMAUsers,P.RakeFingers,P.NumberTxAntennas,P.NumberRxAntennas,P.MIMODetectorType);
+%     semilogy(SNRRange,simulator(P),'DisplayName',name);
+%     hold on;
+% end
+% xlabel('SNR [dB]','FontSize',12,'FontWeight','bold');
+% ylabel('BER','FontSize',12,'FontWeight','bold');
+% xlim([min(SNRRange) max(SNRRange)]);
+% grid minor;
+% legend('-DynamicLegend');
+
+%% fifth simulation: different detectors
 P.NumberTxAntennas  = 2;        % Number of transmission antennas for MIMO
 P.NumberRxAntennas  = 2;        % Number of receive antennas for MIMO
 P.ChannelType       = 'Multipath';   % Since MIMO is implemented, only multipath is possible (inverting an all-1 matrix gives a singularity)
-P.MIMODetectorType  = 'MMSE';
-P.ChannelLength     = 4;
+P.ChannelLength     = 3;
+P.RakeFingers       = 3;
 P.CDMAUsers         = 2;
 figure();
-fingers = [1, 2, 3, 4];
-for i = 1:length(fingers)
-    finger = fingers(i);
-    P.RakeFingers = finger;
+detectors = ["ZF", "MMSE", "SIC"];
+for i = 1:length(detectors)
+    detector = detectors(i);
+    P.MIMODetectorType = detector;
     name = sprintf('Ch.Len.:%d; Users:%d; Fingers:%d; n_T:%d; n_R:%d Detector:%s',P.ChannelLength,P.CDMAUsers,P.RakeFingers,P.NumberTxAntennas,P.NumberRxAntennas,P.MIMODetectorType);
     semilogy(SNRRange,simulator(P),'DisplayName',name);
     hold on;
