cb6533d51e6e2b27ee2520f957b6ad6d73713897
Frag97 <Frag97@users.noreply.github.com>
2020-06-16 03:36:27 +0200

    checked code and added comments

--- simulator.m
+++ simulator.m
@@ -4,6 +4,17 @@
 % Brian Odermatt, Francesco Gallo
 % May 2020
 
+
+% Indices in the code:
+% To improve readability, any two or more for loops that run over the same
+% variable have the same indices 
+% ii ----> Runs over the users
+% jj ----> Runs over the Rx antennas
+% kk ----> Runs over the Tx antennas
+% mm ----> Runs over the Rake fingers
+% bb ----> Runs over the bits of a string
+
+
 function BER = simulator(P)
 
     if P.CDMAUsers > P.HadLen
@@ -22,8 +33,7 @@ function BER = simulator(P)
     
     % Normalized Hadamard matrix: 
     % normalize so that the total symbol power does not change
-    HadamardMatrix       = hadamard(P.HadLen)/sqrt(P.HadLen);
-    SpreadSequence       = HadamardMatrix;
+    SpreadSequence       = hadamard(P.HadLen)/sqrt(P.HadLen);
     SeqLen               = P.HadLen;
     
     % Total number of bits per frame:
@@ -43,32 +53,30 @@ function BER = simulator(P)
     % Convolutional encoder
     ConvolutionalGeneratorPolynoms = [753 561];
     encoder = comm.ConvolutionalEncoder(...
-        'TerminationMethod', 'Terminated',...
-        'TrellisStructure', poly2trellis(ConstraintLength, ConvolutionalGeneratorPolynoms)...
-    );
+        'TerminationMethod', 'Terminated', 'TrellisStructure', ...
+         poly2trellis(ConstraintLength, ConvolutionalGeneratorPolynoms) );
     
     % Convolutional decoder
     decoder = comm.ViterbiDecoder(...
-        'TerminationMethod', 'Terminated',...
-        'TrellisStructure', poly2trellis(ConstraintLength, ConvolutionalGeneratorPolynoms)...
-    );
+        'TerminationMethod', 'Terminated', 'TrellisStructure', ...
+         poly2trellis(ConstraintLength, ConvolutionalGeneratorPolynoms) );
     
     % Channel
     switch P.ChannelType
         case 'Multipath'
             % The convolution of the noisy symbols with channel impulse
             % response adds a convolution tail:
-            NumOfRXChipsPerUser = NumOfChipsPerUser+P.ChannelLength-1;
+            NumOfRxChipsPerUser = NumOfChipsPerUser+P.ChannelLength-1;
         otherwise
-            NumOfRXChipsPerUser = NumOfChipsPerUser;
+            NumOfRxChipsPerUser = NumOfChipsPerUser;
     end
     
     % Initialize variable for error counting
     Results = zeros(1,length(P.SNRRange));
 
-    for ii = 1:P.NumberOfFrames
+    for n = 1:P.NumberOfFrames
 
-        disp(['Simulating: ' sprintf('%.2f', ii/P.NumberOfFrames*100) '%'])
+        disp(['Simulating: ' sprintf('%.2f', n/P.NumberOfFrames*100) '%'])
         
         %% Transmitter
         
@@ -78,8 +86,8 @@ function BER = simulator(P)
 
         % Convolutional encoding: rate 1/2
         EncBits = zeros(NumOfEncBits/Users, Users);
-        for i = 1:Users
-            EncBits(:,i) = step(encoder, Bits(:,i));
+        for ii = 1:Users
+            EncBits(:,ii) = step(encoder, Bits(:,ii));
         end
 
         % BPSK Modulation:
@@ -88,26 +96,44 @@ function BER = simulator(P)
         symbols = 1 - 2*EncBits;
 
         % Orthogonal spreading
-        % Add here explanation on dimensions
+        % dimensions of txsymbols:
+        % (SeqLen x Users) x (Users x NumOfEncBits/Users) = 
+        %          SeqLen  x  NumOfEncBits/Users =
         txsymbols = SpreadSequence(:, 1:Users) * symbols.';
         
         % Applying PN sequence
-        % Add here explanation on dimensions: elementwise multiplication
+        % txsymbols(:) reshapes all elements of A into a single column 
+        % vector, acting columnwise. Dimension:
+        % (SeqLen*NumOfEncBits/Users) x 1 = NumOfChipsPerUser x 1
+        % PNSequence   has dimension:       NumOfChipsPerUser x 1
+        % So, waveform has dimension:       NumOfChipsPerUser x 1
         waveform = txsymbols(:).*PNSequence;
 
-        % Reshape to add multi-user antenna suppport:
-        % Add here explanation on dimensions
+       
+        % The following operation is equivalent to the (non-conjugate)
+        % transpose operation:
         waveform  = reshape(waveform, 1, NumOfChipsPerUser);
+        
+        % Add multi-user and multi-TxAntenna suppport
+        % Dimensions are now:
+        % NumberTxAntennas x NumOfChipsPerUser x Users
         mwaveform = repmat(waveform,[P.NumberTxAntennas 1 Users]);
 
-        
+         
         %% Channel and Noise Realizations
 
         switch P.ChannelType
+            case 'Bypass'
+                error('Channel not supported yet')
+
+            case 'AWGN'
+                error('Channel not supported yet')
+
+            case 'Singlepath Fading'
+                error('Channel not supported yet')
+            
             case 'Multipath'
                 % MIMO multipath channel matrix 
-                % Add here explanation on dimensions
-                % Add explanation on distribution of channel coefficients
                 H = sqrt(1/2) * (...
                     randn(P.ChannelLength * P.NumberRxAntennas, P.NumberTxAntennas, Users) + ...
                     1i * randn(P.ChannelLength * P.NumberRxAntennas, P.NumberTxAntennas, Users));
@@ -117,9 +143,8 @@ function BER = simulator(P)
 
         % Noise initialization: 
         % independent gaussian entries with unit variance (unit average power)
-        % Add here explanation on dimensions
-        snoise = randn(P.ChannelLength * P.NumberRxAntennas, NumOfRXChipsPerUser, Users) + ...
-                 1i * randn(P.ChannelLength * P.NumberRxAntennas, NumOfRXChipsPerUser, Users);
+        snoise = randn(P.ChannelLength * P.NumberRxAntennas, NumOfRxChipsPerUser, Users) + ...
+                 1i * randn(P.ChannelLength * P.NumberRxAntennas, NumOfRxChipsPerUser, Users);
 
         % SNR Range
         for ss = 1:length(P.SNRRange)
@@ -134,26 +159,36 @@ function BER = simulator(P)
             noise  = norm_factor * snoise;
 
             %% Channel Transmission
+            
             switch P.ChannelType
+                
+                case 'Bypass'
+                    error('Channel not supported yet')
+
+                case 'AWGN'
+                    error('Channel not supported yet')
+
+                case 'Singlepath Fading'
+                    error('Channel not supported yet')
+                
                 case 'Multipath'
-                    
                     % Add here explanation on dimensions
-                    y = zeros(P.NumberRxAntennas, NumOfChipsPerUser+P.ChannelLength-1, Users);
+                    y = zeros(P.NumberRxAntennas, NumOfRxChipsPerUser, Users);
                     
-                    for i = 1:Users
-                        % Reshape into the form of eq. 2 of the assignment
-                        % Add here explanation on dimensions
-                        HConv = reshape(H(:,:,i), [P.ChannelLength, P.NumberRxAntennas, P.NumberTxAntennas]);
+                    for ii = 1:Users
+                        % Reshape to consider physical antennas
+                        HConv = reshape(H(:,:,ii), [P.ChannelLength, P.NumberRxAntennas, P.NumberTxAntennas]);
                         
-                        for j = 1:P.NumberRxAntennas
+                        for jj = 1:P.NumberRxAntennas
                             
-                            for k = 1:P.NumberTxAntennas
-                                % perform convolution as in eq. 5/6 of the assignment
-                                h_jk = HConv(:,j,k);
-                                y(j,:,i) = y(j,:,i) + conv(mwaveform(k,:,i), h_jk);
+                            % perform MIMO multipath channel convolution
+                            for kk = 1:P.NumberTxAntennas
+                                h_jk = HConv(:,jj,kk);
+                                y(jj,:,ii) = y(jj,:,ii) + conv(mwaveform(kk,:,ii), h_jk);
                             end
                             
-                            y(j,:,i) = y(j,:,i) + noise(j,:,i);
+                            % add noise
+                            y(jj,:,ii) = y(jj,:,ii) + noise(jj,:,ii);
                             
                         end
                     end
@@ -169,51 +204,68 @@ function BER = simulator(P)
             % each row represents a user string
             RxBits = zeros(Users, NumOfBits/Users);
             
-            for i = 1:Users
+            for ii = 1:Users
                 % first split up into virtual RAKE antennas. There are
                 % P.NumberRxAntennas*P.ChannelLength virtual antennas per user
-                UserSequence = SpreadSequence(:,i); 
+                UserSequence = SpreadSequence(:,ii) * SeqLen; 
+                    % NOTE: Despreading should bring the signal back to
+                    % unit power (in a bypass situation).
+                    % Here the rx signal power without SeqLen coefficient gets
+                    % decreased by a total factor of 64, that's why I
+                    % would multiply by 64 to get again a total average
+                    % power of 1 for the signal
                 rakeAntennas = zeros(P.NumberRxAntennas * P.ChannelLength, NumOfEncBits/Users);
-                for k = 1:P.NumberRxAntennas
-                    for m = 1:P.RakeFingers
-                        data = y(k,m:m+NumOfChipsPerUser-1,i) ./ PNSequence.';
-                        rxvecs  = reshape(data,SeqLen,NumOfEncBits/Users);
-                        rakeAntennas((k-1)*P.RakeFingers + m, :) = UserSequence.' * rxvecs;
+                
+                for jj = 1:P.NumberRxAntennas
+                    for mm = 1:P.RakeFingers
+                        
+                        % Remove PN sequence
+                        % data has dimensions: NumOfChipsPerUser x 1
+                        data = y(jj, mm:mm+NumOfChipsPerUser-1, ii) ./ PNSequence.';
+                        
+                        % Reshape to apply despreading 
+                        rxvecs  = reshape(data, [SeqLen, NumOfEncBits/Users]);
+                        
+                        % Orthogonal despreading:
+                        % each despreading operation gives rise to a 
+                        % (1 x NumOfEncBits/Users) vector
+                        rakeAntennas((jj-1)*P.RakeFingers + mm, :) = UserSequence.' * rxvecs;
+                        
                     end
                 end
             
                 % MIMO with the virtual RAKE antennas directly gives the
-                % estimation of the sent signal on each antenna
-                H_User = squeeze(H(:,:,i));
+                % estimate of the sent signal on each antenna
+                % squeeze removes dimensions of length 1
+                H_User = squeeze(H(:,:,ii));
                 
                 % MIMO detector
                 switch P.MIMODetectorType
                     case 'ZF'
-                        H_H = H_User';
-                        G = inv(H_H * H_User) * H_H;
+                        G = (H_User' * H_User) \ H_User';
                         sTilde = G * rakeAntennas;
                         
                     case 'MMSE'
-                        H_H = H_User';
-                        mult = H_H * H_User;
-                        G = inv(mult + P.NumberTxAntennas*SNRlin*eye(size(mult))) * H_H;
+                        A = H_User' * H_User;
+                        B = P.NumberTxAntennas*SNRlin*eye(size(A));
+                        G = (A + B) \ H_User';
                         sTilde = G * rakeAntennas;
                         
                     case 'SIC'
                         yi = rakeAntennas;
                         Hi = H_User;
-                        Constellations = [-1 1];    % here, the signal is BPSK
-                        for i = 1:P.NumberTxAntennas
-                            Hi_H = Hi';
-                            Hi_inv = inv(Hi_H * Hi) * Hi_H;
-                            g1i_star = Hi_inv(1,:);
+                        % BPSK modulation
+                        Constellations = [-1 1];   
+                        for kk = 1:P.NumberTxAntennas
+                            Gi = (Hi' * Hi) \ Hi';
+                            g1i_star = Gi(1,:);
                             % here we actually produce sHats and not sTilde
-                            for kk = 1:NumOfEncBits/Users
+                            for bb = 1:NumOfEncBits/Users
                                 temp = g1i_star*yi;
-                                [~, closestIndex] = min(temp(kk) - Constellations);
-                                sTilde(i,kk) = Constellations(closestIndex);
+                                [~, closestIndex] = min(temp(bb) - Constellations);
+                                sTilde(kk,bb) = Constellations(closestIndex);
                             end
-                            yi = yi - Hi(:,1) * sTilde(i);
+                            yi = yi - Hi(:,1) * sTilde(kk);
                             Hi(:,1) = [];
                         end
                         
@@ -232,19 +284,21 @@ function BER = simulator(P)
                 decodedBits = step(decoder, real(mrc).');
                 
                 % Eliminating convolution tails
-                RxBits(i,:) = decodedBits(1:P.BitsPerUser);
+                RxBits(ii,:) = decodedBits(1:P.BitsPerUser);
             end
 
             % Flatten the bit vectors for BER count
-            Bits    = reshape(Bits, NumOfBits, 1);
+            Bits    = reshape(Bits,     NumOfBits, 1);
             RxBits  = reshape(RxBits.', NumOfBits, 1);
 
-            % BER count
+            % Error count
             errors      = sum(RxBits ~= Bits);
+            % Add to the errors found in the previous frames
             Results(ss) = Results(ss) + errors;
 
         end
     end
-
+    
+    % Calculate the BER
     BER = Results/(NumOfBits*P.NumberOfFrames);
 end
